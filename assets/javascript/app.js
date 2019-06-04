a5250e8bc81a0e7bac83c3d51e55e3f131ccecda
ToneDef00 <daniels@dual4t.com>
2019-06-04 15:26:13 -0700

    linked HTML to javascript. added submit function. added default value setting for dates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 // key:GBIwIdUd9LHfAwQNE14JT5BeLpePhLAD
 $(document).ready(function(){
-    var input = ;
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + input + "&api-key=GBIwIdUd9LHfAwQNE14JT5BeLpePhLAD";
-    console.log(queryURL);
+
+    //var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + input + "&api-key=GBIwIdUd9LHfAwQNE14JT5BeLpePhLAD";
+    //console.log(queryURL);
     // https://api.nytimes.com/svc/books/v3/lists.json
 // get request
 
@@ -13,9 +13,11 @@ $(document).ready(function(){
 //Search (on click)
 // clear results
 let getAjax = function(search,number,start,end){
+    console.log(search);
 
     let buildURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M&q=${search}&begin_date=${start}&end_date=${end}`
 
+    console.log(buildURL);
     $.ajax({
         url: buildURL,
         method: "GET"
@@ -35,11 +37,21 @@ $("#submit").on("click", function (event) {
     event.preventDefault();
 
     // This line grabs the input from the textbox
-    let searchTerm = $("#search").val().trim();
+    let searchTerm = $("#search").val().trim().replace(/\s/g, "+");;
     let noOfResults = $("#noOfResults").val().trim();
+    
     let startDate = $("#startDate").val().trim();
     let endDate = $("#endDate").val().trim();
-
+    // if(startDate )
+    // let startDate =20180202;
+    // let endDate = 20190101;
+    if(startDate == ""){
+        console.log("blankstring")
+        startDate = 20180202;
+    }
+    if(endDate == ""){
+        endDate = 20180202;
+    }
     getAjax(searchTerm, noOfResults, startDate, endDate)
     
   });
