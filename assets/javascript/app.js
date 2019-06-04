5b1c58e4593bf6d5ba536397b283021ede8571fc
Badri Vasistha <badri.vasistha@gmail.com>
2019-06-04 14:32:54 -0700

    query url added on js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+// key:GBIwIdUd9LHfAwQNE14JT5BeLpePhLAD
+$(document).ready(function(){
+    var input = 
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + input + "&api-key=GBIwIdUd9LHfAwQNE14JT5BeLpePhLAD";
+    console.log(queryURL);
+    // https://api.nytimes.com/svc/books/v3/lists.json
+// get request
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response){
+        console.log(response)
+    })
+// search term textbox
+//Number of Records to Retrieve textbox, input
+//Start Year, input box
+//End Year
+//Search (on click)
+// clear results
+
+//Output Top Articles
+
+
+})
